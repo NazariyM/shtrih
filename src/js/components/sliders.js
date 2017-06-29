3888eb3143022a83e0de4d463e1a82f00bc0d089
NazariyM <nazarriko@gmail.com>
2017-06-29 14:34:00 +0300

    up

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,6 +1,7 @@
 const $testimonialsSld = $('.js-testimonials-slider');
 const $teamSld = $('.js-team-slider');
 const $achieveSld = $('.js-achieve-slider');
+const $qualitySld = $('.js-quality-slider');
 $testimonialsSld.owlCarousel({
   items: 1,
   loop: true,
@@ -57,4 +58,28 @@ $achieveSld.owlCarousel({
       margin: 30
     }
   }
+});
+$qualitySld.owlCarousel({
+  items: 1,
+  loop: true,
+  dots: false,
+  nav: true,
+  smartSpeed: 1000,
+  navText: ['<svg class="owl-carousel__btn-icon icon-sld-arr-lg_l"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/sprite.svg#icon-sld-arr-lg_l"></use></svg>', '<svg class="owl-carousel__btn-icon icon-sld-arr-lg_r"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/sprite.svg#icon-sld-arr-lg_r"></use></svg>'],
+  navClass: ['owl-carousel__btn owl-carousel__btn_prev', 'owl-carousel__btn owl-carousel__btn_next']
+  // responsive: {
+  //   0: {
+  //     items: 1
+  //   },
+  //   767: {
+  //     items: 2
+  //   },
+  //   1023: {
+  //     items: 3,
+  //     margin: 10
+  //   },
+  //   1365: {
+  //     margin: 30
+  //   }
+  // }
 });
